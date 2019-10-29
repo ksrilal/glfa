aad0d374b7184ff52a0d53b1e88e5661fb1174f8
Ilthizam <ilthizambest@gmail.com>
2019-10-30 00:56:36 +0530

    firebase intialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { RedeemTicketComponent } from './redeem-ticket/redeem-ticket.component';
 import { UserCardComponent } from './user-card/user-card.component';
 import { FilterSalesComponent } from './filter-sales/filter-sales.component';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { environment } from 'src/environments/environment.prod';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,10 +29,13 @@ import { FilterSalesComponent } from './filter-sales/filter-sales.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     RouterModule.forRoot([
-      {path:'staff', component: StaffManagementComponent},
-      {path:'redeem', component: RedeemTicketComponent},
-      {path:'filter-sales', component: FilterSalesComponent}
+      {path: 'staff', component: StaffManagementComponent},
+      {path: 'redeem', component: RedeemTicketComponent},
+      {path: 'filter-sales', component: FilterSalesComponent}
     ])
 
   ],
