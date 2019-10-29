a4cf490d3155e852a6651b00ecd2e4944d3a4c4b
Ilthizam <ilthizambest@gmail.com>
2019-10-29 23:48:56 +0530

    basic staff management and ticket redeem page created with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { StaffManagementComponent } from './staff-management/staff-management.component';
+import {RouterModule} from '@angular/router';
+import { RedeemTicketComponent } from './redeem-ticket/redeem-ticket.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    StaffManagementComponent,
+    RedeemTicketComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'staff', component: StaffManagementComponent},
+      {path:'redeem', component: RedeemTicketComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
