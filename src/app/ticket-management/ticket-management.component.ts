7fe8c769b24ba204f3c89abf74d6fddbfaa15e5f
Ilthizam <ilthizambest@gmail.com>
2019-10-30 03:25:28 +0530

    small changers

--- src/app/ticket-management/ticket-management.component.ts
+++ src/app/ticket-management/ticket-management.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { EventService } from '../event.service';
+import { Component, OnInit } from "@angular/core";
+import { EventService } from "../event.service";
 
 @Component({
-  selector: 'app-ticket-management',
-  templateUrl: './ticket-management.component.html',
-  styleUrls: ['./ticket-management.component.scss']
+  selector: "app-ticket-management",
+  templateUrl: "./ticket-management.component.html",
+  styleUrls: ["./ticket-management.component.scss"]
 })
 export class TicketManagementComponent implements OnInit {
-
   events: any[];
 
   constructor(eventService: EventService) {
-    eventService.getAll().subscribe(events=>{
+    eventService.getAll().subscribe(events => {
       this.events = events;
-      console.log(this.events);
     });
-   }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {}
 }
