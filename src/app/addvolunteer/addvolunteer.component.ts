79f367a9d7a9877dd9f7e6402b6a3589b8ca33b2
RusJ <rjillesingha@gmail.com>
2020-01-07 22:25:09 +0530

    fixed some errors in add volunteer form validations

--- src/app/addvolunteer/addvolunteer.component.ts
+++ src/app/addvolunteer/addvolunteer.component.ts
@@ -8,33 +8,35 @@ import { PasswordValidators } from '../validators/password-validator';
   selector: 'app-addvolunteer',
   templateUrl: './addvolunteer.component.html',
   styleUrls: ['./addvolunteer.component.scss']
+  
 })
 export class AddvolunteerComponent implements OnInit {
-
+  volunteers: any[];
   form1 = new FormGroup({
     fName:new FormControl('',Validators.required),
     lName:new FormControl('',Validators.required),
     userName:new FormControl('',Validators.required),
     email: new FormControl('', [Validators.required,Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    passwd: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
     mobile: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
     dob:new FormControl('',Validators.required),
     NIC:new FormControl('',[Validators.required,Validators.maxLength(10)]),
     gender:new FormControl('',Validators.required),
   });
  
- /* constructor(private addVolunteerService:AddVolunteerService) {
-    addVolunteerService.getAll().subscribe(member => {
-      this.members = member;
+ constructor(private addVolunteerService:AddVolunteerService) {
+    addVolunteerService.getAll().subscribe(volunteer => {
+      this.volunteers = volunteer;
     });
   }
 
   onSubmit() {
-    this.addVolunteerService.create(this.form1.value);
+    // this.addVolunteerService.create(this.form1.value);
+    console.log(this.form1.value);
     this.form1.reset();
   }
-*/
-constructor(){}
+
+
    
   get email() {
     return this.form1.get('email');
@@ -71,9 +73,7 @@ constructor(){}
   }
 
 
-  onSubmit() {
 
-  }
 
   revert() {
 
