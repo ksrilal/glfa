2da47016ffd5d9db5be0992a8a4272f30ec4ece3
Ilthizam <ilthizambest@gmail.com>
2020-01-11 13:31:25 +0530

    redeeming functions completed

--- src/app/redeem-ticket/redeem-ticket.component.ts
+++ src/app/redeem-ticket/redeem-ticket.component.ts
@@ -11,7 +11,9 @@ export class RedeemTicketComponent implements OnInit {
   delegateDetials;
   name;
   country;
-  total=0;
+  total = 0;
+  itemCount = 0;
+  orderId;
 
   constructor(private ticketOrderServie: TicketOrderService) {}
 
@@ -19,9 +21,16 @@ export class RedeemTicketComponent implements OnInit {
 
   search(QR) {
     if (QR.value != "") {
+      this.orderId = QR.value;
       this.ticketOrderServie.searchOrder(QR).subscribe(result => {
+        this.total = 0;
+        this.itemCount = 0;
         this.orders = result;
-        this.total=+this.orders.price;
+        result.forEach(element => {
+          this.total += element.price;
+          this.itemCount += element.quantity;
+          // console.log(element);1
+        });
       });
 
       this.ticketOrderServie.searchDeligate(QR).subscribe(delegate => {
@@ -29,6 +38,23 @@ export class RedeemTicketComponent implements OnInit {
         this.name = this.delegateDetials.name;
         this.country = this.delegateDetials.country;
       });
+    }
+  }
+
+  redeemTicket(id) {
+    if (confirm("Are you sure? ")) {
+      // console.log(id);
+      // console.log(this.orderId);
+      this.ticketOrderServie.redeemTicket(this.orderId, id);
+    } else {
+      // console.log("Ugjteobuhoetu");
+    }
+  }
+  redeemAll() {
+    if (confirm("Are you sure? ")) {
+      this.ticketOrderServie.redeemAll(this.orderId, this.orders);
+    } else {
+      // console.log("Ugjteobuhoetu");
     }
   }
 }
