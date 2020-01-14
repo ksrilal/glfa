8373ecdf2768edb37f3dc96b3d20da7de2ae4865
Ilthizam <ilthizambest@gmail.com>
2020-01-14 11:29:00 +0530

    error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { HeaderComponent } from './header/header.component';
-import { StaffManagementComponent } from './staff-management/staff-management.component';
-import {RouterModule} from '@angular/router';
-import { RedeemTicketComponent } from './redeem-ticket/redeem-ticket.component';
-import { UserCardComponent } from './user-card/user-card.component';
-import { FilterSalesComponent } from './filter-sales/filter-sales.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { HeaderComponent } from "./header/header.component";
+import { StaffManagementComponent } from "./staff-management/staff-management.component";
+import { RouterModule } from "@angular/router";
+import { RedeemTicketComponent } from "./redeem-ticket/redeem-ticket.component";
+import { UserCardComponent } from "./user-card/user-card.component";
+import { FilterSalesComponent } from "./filter-sales/filter-sales.component";
 
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireAuthModule} from '@angular/fire/auth';
-import { environment } from 'src/environments/environment.prod';
-import { TicketManagementComponent } from './ticket-management/ticket-management.component';
-import { TicketCardComponent } from './ticket-card/ticket-card.component';
-import { EventService } from './event.service';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { environment } from "src/environments/environment.prod";
+import { TicketManagementComponent } from "./ticket-management/ticket-management.component";
+import { TicketCardComponent } from "./ticket-card/ticket-card.component";
+import { EventService } from "./event.service";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { VolunteermanagementComponent } from './volunteermanagement/volunteermanagement.component';
-import { AddvolunteerComponent } from './addvolunteer/addvolunteer.component';
+import { VolunteermanagementComponent } from "./volunteermanagement/volunteermanagement.component";
+import { AddvolunteerComponent } from "./addvolunteer/addvolunteer.component";
 
-import { AddTasksComponent } from './add-tasks/add-tasks.component';
+import { AddTasksComponent } from "./add-tasks/add-tasks.component";
 
-import { AuthorManagementComponent } from './author-management/author-management.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { EventComponent } from './event/event.component';
-import { LoginComponent } from './login/login.component';
-import { AuthService } from './auth.service';
-import { AuthGuardService } from './auth-guard.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VehicleManagementComponent } from './vehicle-management/vehicle-management.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { AllVehiclesComponent } from './vehicle-management/all-vehicles/all-vehicles.component';
-import { AssignedVehiclesComponent } from './vehicle-management/assigned-vehicles/assigned-vehicles.component';
-import { FreeVehiclesComponent } from './vehicle-management/free-vehicles/free-vehicles.component';
-import { RequestVolunteersComponent } from './request-volunteers/request-volunteers.component';
-import { ContactDriversComponent } from './contact-drivers/contact-drivers.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthorManagementComponent } from "./author-management/author-management.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { EventComponent } from "./event/event.component";
+import { LoginComponent } from "./login/login.component";
+import { AuthService } from "./auth.service";
+import { AuthGuardService } from "./auth-guard.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { VehicleManagementComponent } from "./vehicle-management/vehicle-management.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { AllVehiclesComponent } from "./vehicle-management/all-vehicles/all-vehicles.component";
+import { AssignedVehiclesComponent } from "./vehicle-management/assigned-vehicles/assigned-vehicles.component";
+import { FreeVehiclesComponent } from "./vehicle-management/free-vehicles/free-vehicles.component";
+import { RequestVolunteersComponent } from "./request-volunteers/request-volunteers.component";
+import { ContactDriversComponent } from "./contact-drivers/contact-drivers.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
-import { TodoComponent } from './todo/todo.component';
-import { OngoingComponent } from './ongoing/ongoing.component';
-import { DoneComponent } from './done/done.component';
-
-import { ContactDriversCardComponent } from './contact-drivers-card/contact-drivers-card.component';
-
-import { ContactDriversService } from './contact-drivers.service';
-
-import { AuthorService } from './author-management-service/author.service';
-
-import { RequestVolunteersService } from './request-volunteers.service';
-import { VehicleManagementService } from './vehicle-management.service';
-
-
-
-
-
-
+import { ContactDriversService } from "./contact-drivers.service";
+import { Ng2SmartTableModule } from "ng2-smart-table";
+import { Ng2CompleterModule } from "ng2-completer";
+import { TodoComponent } from "./todo/todo.component";
+import { OngoingComponent } from "./ongoing/ongoing.component";
+import { DoneComponent } from "./done/done.component";
+import { RequestVolunteersService } from "./request-volunteers.service";
+import { VehicleManagementService } from "./vehicle-management.service";
+import { AuthorService } from "./author-management-service/author.service";
+import { ContactDriversCardComponent } from "./contact-drivers-card/contact-drivers-card.component";
 
 @NgModule({
   declarations: [
@@ -72,9 +64,7 @@ import { VehicleManagementService } from './vehicle-management.service';
     TicketCardComponent,
     VolunteermanagementComponent,
     AddvolunteerComponent,
-
     AddTasksComponent,
-
     AuthorManagementComponent,
     EventComponent,
     LoginComponent,
@@ -86,16 +76,12 @@ import { VehicleManagementService } from './vehicle-management.service';
     ContactDriversComponent,
     DashboardComponent,
 
-
     TodoComponent,
     OngoingComponent,
-    DoneComponent
-
+    DoneComponent,
     ContactDriversCardComponent
-
-
   ],
-  
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -160,39 +146,46 @@ import { VehicleManagementService } from './vehicle-management.service';
       {
         path: "vehicle-management",
         component: VehicleManagementComponent
-
       },
-      { path: "vehicle-management/all-vehicles",
+      {
+        path: "vehicle-management/all-vehicles",
         component: AllVehiclesComponent
       },
 
-      { path: "vehicle-management/free-vehicles",
+      {
+        path: "vehicle-management/free-vehicles",
         component: FreeVehiclesComponent
       },
-      { path: "vehicle-management/assigned-vehicles",
+      {
+        path: "vehicle-management/assigned-vehicles",
         component: AssignedVehiclesComponent
       },
       {
         path: "request-volunteers",
         component: RequestVolunteersComponent
-
       },
       {
         path: "contact-drivers",
         component: ContactDriversComponent
-
       }
     ]),
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    Ng2SmartTableModule,
+    Ng2CompleterModule
   ],
-
-
-
-  providers: [EventService, AuthService, AuthGuardService, ContactDriversService, RequestVolunteersService, VehicleManagementService,AuthorService],
+  providers: [
+    EventService,
+    AuthService,
+    AuthGuardService,
+    ContactDriversService,
+    RequestVolunteersService,
+    VehicleManagementService,
+    AuthorService
+  ],
 
   bootstrap: [AppComponent]
 })
--- src/app/filter-sales/filter-sales.component.ts
+++ src/app/filter-sales/filter-sales.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SalesService } from "./sales.service";
 
 @Component({
-  selector: 'app-filter-sales',
-  templateUrl: './filter-sales.component.html',
-  styleUrls: ['./filter-sales.component.scss']
+  selector: "app-filter-sales",
+  templateUrl: "./filter-sales.component.html",
+  styleUrls: ["./filter-sales.component.scss"]
 })
 export class FilterSalesComponent implements OnInit {
+  settings = {
+    columns: {
+      // id: {
+      //   title: "ID"
+      // },
+      name: {
+        title: "Full Name"
+      },
+      country: {
+        title: "Country"
+      },
+      event: {
+        title: "Event"
+      },
+      author: {
+        title: "Author"
+      },
+      quantity: {
+        title: "Quantity"
+      },
 
-  constructor() { }
+      status: {
+        title: "Redeem Status"
+      }
+    }
+  };
+  data;
 
-  ngOnInit() {
+  constructor(salesService: SalesService) {
+    salesService.getAll().subscribe(data => {
+      this.data = data;
+      console.log(data);
+    });
   }
 
+  ngOnInit() {}
 }
