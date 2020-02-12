a57d5b02ef348c1fe364ff6c04b53d923d2f127a
RusJ <rjillesingha@gmail.com>
2020-02-12 12:55:28 +0530

    volunteer coordinator

--- Version 2.0/src/app/pages/task-management/task-management.module.ts
+++ Version 2.0/src/app/pages/task-management/task-management.module.ts
@@ -1,9 +1,35 @@
 import { NgModule } from "@angular/core";
 import { TaskManagementRoutingModule, routedComponents } from './task-management-routing.module';
-
+import { ThemeModule } from '../../@theme/theme.module';
+import { NbInputModule, NbCardModule, NbButtonModule, NbActionsModule, NbUserModule, NbCheckboxModule, NbRadioModule, NbDatepickerModule, NbSelectModule, NbIconModule } from '@nebular/theme';
+import { FormsRoutingModule } from '../forms/forms-routing.module';
+import { FormsModule as ngFormsModule } from '@angular/forms';
+import { FormsComponent } from '../forms/forms.component';
+import { ButtonsComponent } from '../forms/buttons/buttons.component';
+import { FormInputsComponent } from '../forms/form-inputs/form-inputs.component';
+import { FormLayoutsComponent } from '../forms/form-layouts/form-layouts.component';
+import { DatepickerComponent } from '../forms/datepicker/datepicker.component';
 
 @NgModule({
-  imports: [TaskManagementRoutingModule],
-  declarations: [...routedComponents]
+  imports: [TaskManagementRoutingModule,
+    ThemeModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    FormsRoutingModule,
+    NbSelectModule,
+    NbIconModule,
+    ngFormsModule,],
+  declarations: [...routedComponents,
+    FormsComponent,
+    ButtonsComponent,
+    FormInputsComponent,
+    FormLayoutsComponent,
+    DatepickerComponent,]
 })
 export class TaskManagementModule {}
