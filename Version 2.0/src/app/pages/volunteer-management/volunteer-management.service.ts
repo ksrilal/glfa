7440660564ae50f479b25aad504ed9b19d1bbb3b
RusJ <rjillesingha@gmail.com>
2020-02-14 10:36:46 +0530

    Some small changes

--- Version 2.0/src/app/pages/volunteer-management/volunteer-management.service.ts
+++ Version 2.0/src/app/pages/volunteer-management/volunteer-management.service.ts
@@ -7,14 +7,14 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class VolunteerManagementService {
 
   
-  constructor(private afs: AngularFirestore, private authService: AuthService) { 
+  constructor(private afs: AngularFirestore, /*private authService: AuthService*/) { 
     console.log("in services",this.getAll());
   }
 
   create(volunteer) {
 
     try{
-       this.authService.signup(volunteer.email,volunteer.passwd);
+      // this.authService.signup(volunteer.email,volunteer.passwd);//leave this only
       // this.auth.auth.createUserWithEmailAndPassword(staff.email,staff.password);
       this.afs.collection('users').doc(volunteer.email).set(volunteer);
       alert("Addedd Successfully");
