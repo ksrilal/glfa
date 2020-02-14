531e3781c52518f2b0be7cd19807b64037704c1d
Ilthizam <ilthizambest@gmail.com>
2020-02-14 15:04:32 +0530

    transpotation hat

---
+++ Version 2.0/src/app/pages/transpotation-chat/transpotation-chat.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "ngx-transpotation-chat",
+  template: `
+    <router-outlet></router-outlet>
+  `
+})
+export class TranspotationChatComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
