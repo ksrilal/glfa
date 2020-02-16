24f3707954ea3236367ee14b61e38471d287ab94
Ilthizam <ilthizambest@gmail.com>
2020-02-16 16:06:42 +0530

    modify box office cmplete

--- Version 2.0/src/app/pages/driver-management/modify-drivers/modify-drivers.component.ts
+++ Version 2.0/src/app/pages/driver-management/modify-drivers/modify-drivers.component.ts
@@ -1,74 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
-import { SmartTableData } from '../../../@core/data/smart-table';
-import { DriverManagementService } from '../driver-management.service';
+import { Component, OnInit } from "@angular/core";
+import { LocalDataSource } from "ng2-smart-table";
+import { SmartTableData } from "../../../@core/data/smart-table";
+import { DriverManagementService } from "../driver-management.service";
 
 @Component({
-  selector: 'ngx-modify-drivers',
-  templateUrl: './modify-drivers.component.html',
-  styleUrls: ['./modify-drivers.component.scss']
+  selector: "ngx-modify-drivers",
+  templateUrl: "./modify-drivers.component.html",
+  styleUrls: ["./modify-drivers.component.scss"]
 })
 export class ModifyDriversComponent implements OnInit {
   ngOnInit(): void {
     throw new Error("Method not implemented.");
   }
   settings = {
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
+    actions: {
+      add: false
     },
     edit: {
       editButtonContent: '<i class="nb-edit"></i>',
       saveButtonContent: '<i class="nb-checkmark"></i>',
       cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave:true,
+      confirmSave: true
     },
     delete: {
       deleteButtonContent: '<i class="nb-trash"></i>',
-      confirmDelete: true,
+      confirmDelete: true
     },
     columns: {
       nic: {
-        title: 'NIC',
-        type: 'string',
+        title: "NIC",
+        type: "string"
       },
       fname: {
-        title: 'First Name',
-        type: 'string',
+        title: "First Name",
+        type: "string"
       },
       lname: {
-        title: 'Last Name',
-        type: 'string',
+        title: "Last Name",
+        type: "string"
       },
       vehbrand: {
-        title: 'Vehicle Brand',
-        type: 'string',
+        title: "Vehicle Brand",
+        type: "string"
       },
       vehmodel: {
-        title: 'Vehicle  Model',
-        type: 'string',
+        title: "Vehicle  Model",
+        type: "string"
       },
       vehnum: {
-        title: 'Vehicle Number',
-        type: 'string',
-      },
-    },
+        title: "Vehicle Number",
+        type: "string"
+      }
+    }
   };
 
   source;
 
-  constructor(private driverManagementService:DriverManagementService) {
-    driverManagementService.getAll().subscribe(result=>{
-      this.source= result
-    })
+  constructor(private driverManagementService: DriverManagementService) {
+    driverManagementService.getAll().subscribe(result => {
+      this.source = result;
+    });
   }
 
-  onSaveConfirm(event):void{
-    if (window.confirm('Are you sure you want to edit?')) {
+  onSaveConfirm(event): void {
+    if (window.confirm("Are you sure you want to edit?")) {
       // event.confirm.resolve();
       // this.driverManagementService.delete(event.data.id)
-      this.driverManagementService.edit(event.data.id,event.newData)
+      this.driverManagementService.edit(event.data.id, event.newData);
       // console.log(event.data)
       // console.log(event.newData)
     } else {
@@ -77,13 +75,12 @@ export class ModifyDriversComponent implements OnInit {
   }
 
   onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm("Are you sure you want to delete?")) {
       // event.confirm.resolve();
-      this.driverManagementService.delete(event.data.id)
+      this.driverManagementService.delete(event.data.id);
       // console.log(event.data.id)
     } else {
       event.confirm.reject();
     }
-  }}
-
-
+  }
+}
